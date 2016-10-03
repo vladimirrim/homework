0b166fe854e5b4a6e81b09dd821205cfeea33b49
vladimirrim <vladimirrim98@gmail.com>
2016-10-03 22:32:53 +0300

    second attempt

--- strassen.py
+++ strassen.py
@@ -1,59 +1,59 @@
 import numpy as np
-import sys
 
 
 def matrix_create():
-    global n, a, b, k
-    n = int(input(sys.stdin))
+    n = int(input())
     k = 2
     while (k < n):
         k *= 2
     a = np.zeros((k, k))
     b = np.zeros((k, k))
     c = np.zeros((n + n, n))
-    c[...] = np.loadtxt(sys.stdin)
-    a[:n, :n] = c[:n, ].copy()
-    b[:n, :n] = c[n:, ].copy()
+    c[...] = np.loadtxt()
+    a[:n, :n] = c[:n, ]
+    b[:n, :n] = c[n:, ]
+    return a, b, k, n
 
 
-def matrix_mull():
-    a11 = a[:k // 2, :k // 2].copy()
-    a12 = a[:k // 2, k // 2:].copy()
-    a21 = a[k // 2:, :k // 2].copy()
-    a22 = a[k // 2:, k // 2:].copy()
-    b11 = b[:k // 2, :k // 2].copy()
-    b12 = b[:k // 2, k // 2:].copy()
-    b21 = b[k // 2:, :k // 2].copy()
-    b22 = b[k // 2:, k // 2:].copy()
-    I = np.dot((a11 + a22), (b11 + b22))
-    II = np.dot((a21 + a22), b11)
-    III = np.dot(a11, (b12 - b22))
-    IV = np.dot(a22, (-b11 + b21))
-    V = np.dot((a11 + a12), b22)
-    VI = np.dot((a21 - a11), (b11 + b12))
-    VII = np.dot((a12 - a22), (b21 + b22))
-    c11 = I + IV - V + VII
-    c21 = II + IV
-    c12 = III + V
-    c22 = I + III - II + VI
-    c = np.zeros((k, k), dtype=int)
-    c[:k // 2, :k // 2] = c11.copy()
-    c[:k // 2, k // 2:] = c12.copy()
-    c[k // 2:, :k // 2] = c21.copy()
-    c[k // 2:, k // 2:] = c22.copy()
-    c = c[:n, :n].copy()
-    return c
+def matrix_mull(a, b, k, n):
+    if k <= 1:
+        return np.dot(a, b)
+    else:
+        k = k // 2
+        a11 = a[:k, :k]
+        a12 = a[:k, k:]
+        a21 = a[k:, :k]
+        a22 = a[k:, k:]
+        b11 = b[:k, :k]
+        b12 = b[:k, k:]
+        b21 = b[k:, :k]
+        b22 = b[k:, k:]
+        I = matrix_mull(a11 + a22, b11 + b22, k, n)
+        II = matrix_mull(a21 + a22, b11, k, n)
+        III = matrix_mull(a11, b12 - b22, k, n)
+        IV = matrix_mull(a22, b21 - b11, k, n)
+        V = matrix_mull(a11 + a12, b22, k, n)
+        VI = matrix_mull(a21 - a11, b11 + b12, k, n)
+        VII = matrix_mull(a12 - a22, b21 + b22, k, n)
+
+        c12 = III + V
+        c21 = II + IV
+        c11 = I + IV - V + VII
+        c22 = I + III - II + VI
+
+        c = np.zeros((2 * k, 2 * k))
+        c[:k, :k] = c11
+        c[:k, k:] = c12
+        c[k:, :k] = c21
+        c[k:, k:] = c22
+        c = c[:n, :n]
+        return c
 
 
 def matrix_print(c):
-    for i in range(n):
-        for j in range(n):
-            print(c[i, j], end=" ")
-
-        print()
+    print('\n'.join([' '.join([str(i) for i in j]) for j in c]))
 
 
-matrix_create()
-matrix_mull()
-matrix_print(matrix_mull())
+a, b, k, n = matrix_create()
+matrix_print(matrix_mull(a, b, k, n))
 
